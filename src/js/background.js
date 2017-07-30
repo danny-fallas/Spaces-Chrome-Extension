792eea5f29622181d79c3240265a586b827cd34e
Danny Fallas <dann_1990@hotmail.com>
2017-07-30 00:20:04 -0600

    First functional code

--- src/js/background.js
+++ src/js/background.js
@@ -1,63 +1,58 @@
 'use strict';
+var space = [];
+var newTab = "chrome://newtab/";
 
-var isActive = false;
-var activeOnce = false;
-var _tabId = null;
-var images = [];
-var imagesReady = false;
+function SaveCurrentSpace() {
+    chrome.tabs.create({ "url": newTab });
+    chrome.tabs.query({ "currentWindow": true }, callback);
 
-function startService() {
-    chrome.browserAction.setIcon({ path: 'media/on.png' });
-    chrome.browserAction.setTitle({ title: 'Turn OFF' });
-    runScript({ code: 'start()' });
-    activeOnce = true;
-}
-
-function stopService() {
-    chrome.browserAction.setIcon({ path: 'media/off.png' });
-    chrome.browserAction.setTitle({ title: 'Turn ON' });
-    runScript({ code: 'stop()' });
-    activeOnce = false;
-}
-
-function changeStatus(tabId) {
-    _tabId = tabId;
-    isActive = !isActive;
-
-    if (isActive) {
-        startService();
-        if (imagesReady)
-            sendMessageToTabs(images);
+    function callback(tabs) {
+        tabs.forEach(function (currentValue, index, array) {
+            SaveAndCloseTab(currentValue);
+        });
     }
-    else {
-        stopService();
-        chrome.tabs.reload(_tabId);
+}
+
+function LoadSpace() {
+    space.forEach(function (currentValue, index, array) {
+        chrome.tabs.create(currentValue, callback);
+
+        function callback() {
+            array.splice(index, 1);
+        }
+    });
+
+    space = [];
+}
+
+function SaveAndCloseTab(tab) {
+    if (!tab || (tab && (tab.pinned || tab.url === newTab))) {
+        return;
+    }
+
+    var _tab = {
+        index: tab.index,
+        url: tab.url,
+        active: tab.active,
+        selected: tab.selected,
+    }
+
+    chrome.tabs.remove(tab.id, callback);
+
+    function callback() {
+        space.push(_tab);
     }
 }
 
 chrome.browserAction.onClicked.addListener(function (tab) {
-    changeStatus(tab.Id || tab.id);
-});
-
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-    if (activeOnce && (tab.url && tab.url.toLowerCase().includes('whatsapp')) && (tab.status && tab.status === 'complete')) {
-        isActive = true;
-        startService();
+    if (space.length > 0) {
+        LoadSpace();
+    } else {
+        SaveCurrentSpace();
     }
 });
-
-function sendMessageToTabs(message) {
-    chrome.tabs.sendMessage(_tabId, message, null, null)
-        .then(response => console.log(response))
-        .catch(onError);
-}
 
 function runScript(code) {
-    function callback(response) {
-        var r = response;
-        //console.debug(response);
-    }
-
     try {
         if (code.code)
             chrome.tabs.executeScript(_tabId, code, callback);
@@ -66,22 +61,11 @@ function runScript(code) {
     } catch (e) {
         //console.error('ERROR@chrome.tabs.executeScript: ' + e);
     }
-}
 
-
-function getImages() {
-    const feedURL = 'https://api.unsplash.com/photos?client_id=08aac6aa1c68e14f6e52c4650cd365fa8232f83669850353eba852a566ff3806&per_page=100';
-    fetch(feedURL)
-        .then(response => response.json())
-        .then((items) => {
-            items.forEach(item => images.push(item.urls.small));
-            imagesReady = true;
-        });
+    function callback(response) {
+        console.debug(response);
+    }
 }
 
 //Load content script
 runScript({ file: 'contentscript.js' });
-//Force stop the service on start
-stopService();
-//Get images from API serve them to the extension
-getImages();
